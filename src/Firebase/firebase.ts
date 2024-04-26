a99123317ca793414a351531753e2ea6e1b707b7
DevShubX <60731101+DevShubX@users.noreply.github.com>
2024-04-27 00:49:44 +0530

    Update firebase.ts

--- src/Firebase/firebase.ts
+++ src/Firebase/firebase.ts
@@ -7,13 +7,7 @@ import {getFirestore} from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCUZXSmeYKuduFNHjHruCNrlSyaLZD_m0o",
-  authDomain: "webrtc-app-b70e4.firebaseapp.com",
-  projectId: "webrtc-app-b70e4",
-  storageBucket: "webrtc-app-b70e4.appspot.com",
-  messagingSenderId: "75008862267",
-  appId: "1:75008862267:web:43d873b6407edd7400d41e",
-  measurementId: "G-3NRRTFPSK0"
+  
 };
 
 // Initialize Firebase
