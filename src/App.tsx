dfe1c12df521fdf2f9005d9416e43ef9598d3eb2
DevShubX <60731101+DevShubX@users.noreply.github.com>
2024-07-24 00:34:13 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   const servers = {
     iceServers: [
       {
-        urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+        urls: ['stun:stun.vomessen.de:3478'],
       },
     ],
     iceCandidatePoolSize: 10,
